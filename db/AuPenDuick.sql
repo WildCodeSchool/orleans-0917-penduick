9f976803e5b0e6bdb844ac9bc732ee85b7553cdc
Pierre AMBOISE <pierre.amboise@yahoo.fr>
2017-10-10 17:28:15 +0200

    Rajout nouvelle BDD

--- db/AuPenDuick.sql
+++ db/AuPenDuick.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le :  mer. 04 oct. 2017 à 11:40
+-- Généré le :  mar. 10 oct. 2017 à 17:23
 -- Version du serveur :  5.7.19-0ubuntu0.16.04.1
 -- Version de PHP :  7.0.22-0ubuntu0.16.04.1
 
@@ -23,8 +23,54 @@ SET time_zone = "+00:00";
 --
 
 -- --------------------------------------------------------
-CREATE DATABASE AuPenDuick;
-USE AuPenDuick;
+
+--
+-- Structure de la table `display`
+--
+
+CREATE TABLE `display` (
+  `id` int(11) NOT NULL,
+  `textId` int(11) NOT NULL,
+  `picturesId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `display`
+--
+
+INSERT INTO `display` (`id`, `textId`, `picturesId`) VALUES
+(1, 1, 1),
+(2, 4, 2),
+(3, 5, 2),
+(4, 6, 4),
+(5, 7, 3),
+(6, 8, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `menu`
+--
+
+CREATE TABLE `menu` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `price` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `menu`
+--
+
+INSERT INTO `menu` (`id`, `title`, `description`, `price`) VALUES
+(1, 'Crêpe jambon', 'LE bon jambon bien de chez nous', 5.68),
+(2, 'Crêpe aux petits pois', 'Original, attention à ne pas renverser', 7.77),
+(3, 'Crêpe au sucre', 'A manger sans faim', 6.8),
+(4, 'Crêpe au fraises', 'De saison', 9.25);
+
+-- --------------------------------------------------------
+
 --
 -- Structure de la table `pictures`
 --
@@ -32,41 +78,81 @@ USE AuPenDuick;
 CREATE TABLE `pictures` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `LocalSrc` varchar(255) NOT NULL,
-  `InternetSrc` varchar(255) NOT NULL
+  `localSrc` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `pictures`
 --
 
-INSERT INTO `pictures` (`id`, `name`, `LocalSrc`, `InternetSrc`) VALUES
-(1, 'photo1', '', 'https://www.odelices.com/images/recettes/gaufres.jpg'),
-(2, 'photo2', 'Crepes-bretonnes2-1024x680.png', '');
+INSERT INTO `pictures` (`id`, `name`, `localSrc`) VALUES
+(1, 'bg-crepe', 'bg-crepe.jpg'),
+(2, 'menu-aperitif', 'menu-aperitif.jpg'),
+(3, 'menu-crepes-bretonnes', 'menu-crepes-bretonnes.jpg'),
+(4, 'menu-galettes-bretonnes', 'menu-galettes-bretonnes.jpg'),
+(5, 'menu-desserts', 'menu-desserts.jpg');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `text`
+-- Structure de la table `texts`
 --
 
-CREATE TABLE `text` (
+CREATE TABLE `texts` (
   `id` int(11) NOT NULL,
   `text` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `text`
+-- Déchargement des données de la table `texts`
 --
 
-INSERT INTO `text` (`id`, `text`) VALUES
-(1, 'Bienvenue'),
-(2, 'Voici la carte          '),
-(3, 'A bientôt');
+INSERT INTO `texts` (`id`, `text`) VALUES
+(1, 'Bonjour'),
+(2, 'Voici la carte'),
+(3, 'A bientôt &'),
+(4, 'Découvrez la carte de votre restaurant Au Pen Duick & ses spécialités'),
+(5, 'APÉRITIF & ENTRÉE'),
+(6, 'GALETTES BRETONNES'),
+(7, 'CRÊPES BRETONNES'),
+(8, 'DESSERTS');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `password`) VALUES
+(1, 'philippe-dechaume', 'aupenduick');
 
 --
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `display`
+--
+ALTER TABLE `display`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `Display_Text` (`textId`),
+  ADD KEY `Display_Pictures` (`picturesId`);
+
+--
+-- Index pour la table `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `pictures`
@@ -75,9 +161,15 @@ ALTER TABLE `pictures`
   ADD PRIMARY KEY (`id`);
 
 --
--- Index pour la table `text`
+-- Index pour la table `texts`
 --
-ALTER TABLE `text`
+ALTER TABLE `texts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -85,16 +177,45 @@ ALTER TABLE `text`
 --
 
 --
+-- AUTO_INCREMENT pour la table `display`
+--
+ALTER TABLE `display`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT pour la table `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT pour la table `pictures`
 --
 ALTER TABLE `pictures`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT pour la table `text`
+-- AUTO_INCREMENT pour la table `texts`
 --
-ALTER TABLE `text`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `texts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `display`
+--
+ALTER TABLE `display`
+  ADD CONSTRAINT `Display_Pictures` FOREIGN KEY (`picturesId`) REFERENCES `pictures` (`id`),
+  ADD CONSTRAINT `Display_Text` FOREIGN KEY (`textId`) REFERENCES `texts` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
