ba88ff4fc18203a94148860b5a6313e9946cd4ac
wilder10 <fabien.poher45@gmail.com>
2017-10-18 16:54:14 +0200

    navbar5

--- public/index.php
+++ public/index.php
@@ -12,10 +12,10 @@ if (!empty($_GET['route'])) {
 // On appelle une methode d'un controlleur en fonction de la route saisie en URL
 if ($route == 'home' OR $route == '') {
     $personController = new \AuPenDuick\Controller\HomeController();
-    echo  $personController->homeAction();
+    echo $personController->homeAction();
 } elseif ($route == 'carte') {
     $personController = new \AuPenDuick\Controller\HomeController();
-    echo  $personController->menuContentAction();
+    echo $personController->menuContentAction();
 } else {
     echo 'La page n\'existe pas';
 }
