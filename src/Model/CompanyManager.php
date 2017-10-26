5878d3c78198c5dcec8f5ad77191390d7b955d4f
Pierre AMBOISE <pierre.amboise@yahoo.fr>
2017-10-26 11:04:01 +0200

    Maj

--- src/Model/CompanyManager.php
+++ src/Model/CompanyManager.php
@@ -10,7 +10,7 @@ class CompanyManager extends EntityManager
 {
     public function findAllCompany()
     {
-        $query = "SELECT * FROM company";
+        $query = "SELECT * FROM companyTexts";
         $statement = $this->pdo->query($query);
         return $statement->fetchAll(\PDO::FETCH_CLASS, \AuPenDuick\Model\Company::class);
     }
